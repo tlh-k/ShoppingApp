2dba60ba639eb5f4347486d14135a6a4267d6c3d
tlh-k <mtalhakiyak@gmail.com>
2022-11-07 11:16:29 +0300

    Vue ile ürün sayfası eklendi. (FE)

---
+++ src/components/ProductList.vue
@@ -0,0 +1,285 @@
+<template>
+  <div class="container d-flex justify-content-around">
+    <figure class="card card-product-grid card-lg"> <a href="#" class="img-wrap" data-abc="true"> <img src="https://i.imgur.com/MPqUt62.jpg"> </a>
+      <figcaption class="info-wrap">
+        <div class="row">
+          <div class="col-md-9 col-xs-9"> <a href="#" class="title" data-abc="true">Dell Xtreme 270</a> <span class="rated">Laptops</span> </div>
+          <div class="col-md-3 col-xs-3">
+            <div class="rating text-right"> <i class="fa fa-star"></i> <i class="fa fa-star"></i> <i class="fa fa-star"></i> <i class="fa fa-star"></i> <span class="rated">Rated 4.0/5</span> </div>
+          </div>
+        </div>
+      </figcaption>
+      <div class="bottom-wrap-payment">
+        <figcaption class="info-wrap">
+          <div class="row">
+            <div class="col-md-9 col-xs-9"> <a href="#" class="title" data-abc="true">$3,999</a> <span class="rated">VISA Platinum</span> </div>
+            <div class="col-md-3 col-xs-3">
+              <div class="rating text-right"> #### 8787 </div>
+            </div>
+          </div>
+        </figcaption>
+      </div>
+      <div class="bottom-wrap"> <a href="#" class="btn btn-primary float-right" data-abc="true"> Buy now </a>
+        <div class="price-wrap"> <a href="#" class="btn btn-warning float-left" data-abc="true"> Cancel </a> </div>
+      </div>
+    </figure>
+
+
+
+    <figure class="card card-product-grid card-lg"> <a href="#" class="img-wrap" data-abc="true"> <img src="https://img-monsternotebook.mncdn.com/UPLOAD/urun-gorselleri-yeni/TULPAR/T7_v18/thumb/01-T7_v18-2080_medium.png"> </a>
+      <figcaption class="info-wrap">
+        <div class="row">
+          <div class="col-md-9 col-xs-9"> <a href="#" class="title" data-abc="true">Tulpar T7 V18.3.2 17,3"</a> <span class="rated">Laptops</span> </div>
+          <div class="col-md-3 col-xs-3">
+            <div class="rating text-right"> <i class="fa fa-star"></i> <i class="fa fa-star"></i> <i class="fa fa-star"></i> <i class="fa fa-star"></i> <span class="rated">Rated 4.4/5</span> </div>
+          </div>
+        </div>
+      </figcaption>
+      <div class="bottom-wrap-payment">
+        <figcaption class="info-wrap">
+          <div class="row">
+            <div class="col-md-9 col-xs-9"> <a href="#" class="title" data-abc="true">$4,120</a> <span class="rated">VISA Platinum</span> </div>
+            <div class="col-md-3 col-xs-3">
+              <div class="rating text-right"> #### 9090 </div>
+            </div>
+          </div>
+        </figcaption>
+      </div>
+      <div class="bottom-wrap"> <a href="#" class="btn btn-primary float-right" data-abc="true"> Buy now </a>
+        <div class="price-wrap"> <a href="#" class="btn btn-warning float-left" data-abc="true"> Cancel </a> </div>
+      </div>
+    </figure>
+
+
+
+
+    <figure class="card card-product-grid card-lg"> <a href="#" class="img-wrap" data-abc="true"> <img src="https://m.media-amazon.com/images/I/41VmDUVHBWL._AC_SY780_.jpg"> </a>
+      <figcaption class="info-wrap">
+        <div class="row">
+          <div class="col-md-9 col-xs-9"> <a href="#" class="title" data-abc="true">Acer Aspire 3 A315-56</a> <span class="rated">Laptops</span> </div>
+          <div class="col-md-3 col-xs-3">
+            <div class="rating text-right"> <i class="fa fa-star"></i> <i class="fa fa-star"></i> <i class="fa fa-star"></i> <i class="fa fa-star"></i> <span class="rated">Rated 3.9/5</span> </div>
+          </div>
+        </div>
+      </figcaption>
+      <div class="bottom-wrap-payment">
+        <figcaption class="info-wrap">
+          <div class="row">
+            <div class="col-md-9 col-xs-9"> <a href="#" class="title" data-abc="true">$3,500</a> <span class="rated">VISA Platinum</span> </div>
+            <div class="col-md-3 col-xs-3">
+              <div class="rating text-right"> #### 8220 </div>
+            </div>
+          </div>
+        </figcaption>
+      </div>
+      <div class="bottom-wrap"> <a href="#" class="btn btn-primary float-right" data-abc="true"> Buy now </a>
+        <div class="price-wrap"> <a href="#" class="btn btn-warning float-left" data-abc="true"> Cancel </a> </div>
+      </div>
+    </figure>
+
+  </div>
+
+  <!-- NEW COLUMN -->
+
+  <div class="container d-flex justify-content-around">
+    <figure class="card card-product-grid card-lg"> <a href="#" class="img-wrap" data-abc="true"> <img src="https://cdn.akakce.com/hp/hp-15-da2031nt-9ff59ea-i5-10210u-16-gb-512-gb-ssd-mx130-15-6-full-hd-notebook-z.jpg"> </a>
+      <figcaption class="info-wrap">
+        <div class="row">
+          <div class="col-md-9 col-xs-9"> <a href="#" class="title" data-abc="true">HP 15-DA2031NT</a> <span class="rated">Laptops</span> </div>
+          <div class="col-md-3 col-xs-3">
+            <div class="rating text-right"> <i class="fa fa-star"></i> <i class="fa fa-star"></i> <i class="fa fa-star"></i> <i class="fa fa-star"></i> <span class="rated">Rated 4.1/5</span> </div>
+          </div>
+        </div>
+      </figcaption>
+      <div class="bottom-wrap-payment">
+        <figcaption class="info-wrap">
+          <div class="row">
+            <div class="col-md-9 col-xs-9"> <a href="#" class="title" data-abc="true">$3,800</a> <span class="rated">VISA Platinum</span> </div>
+            <div class="col-md-3 col-xs-3">
+              <div class="rating text-right"> #### 1122 </div>
+            </div>
+          </div>
+        </figcaption>
+      </div>
+      <div class="bottom-wrap"> <a href="#" class="btn btn-primary float-right" data-abc="true"> Buy now </a>
+        <div class="price-wrap"> <a href="#" class="btn btn-warning float-left" data-abc="true"> Cancel </a> </div>
+      </div>
+    </figure>
+
+
+
+    <figure class="card card-product-grid card-lg"> <a href="#" class="img-wrap" data-abc="true"> <img src="https://iis-akakce.akamaized.net/p.x?%2F%2Fproductimages%2Ehepsiburada%2Enet%2Fs%2F44%2F600%2F10790920192050%2Ejpg"> </a>
+      <figcaption class="info-wrap">
+        <div class="row">
+          <div class="col-md-9 col-xs-9"> <a href="#" class="title" data-abc="true">Huawei MateBook X</a> <span class="rated">Laptops</span> </div>
+          <div class="col-md-3 col-xs-3">
+            <div class="rating text-right"> <i class="fa fa-star"></i> <i class="fa fa-star"></i> <i class="fa fa-star"></i> <i class="fa fa-star"></i> <span class="rated">Rated 4.8/5</span> </div>
+          </div>
+        </div>
+      </figcaption>
+      <div class="bottom-wrap-payment">
+        <figcaption class="info-wrap">
+          <div class="row">
+            <div class="col-md-9 col-xs-9"> <a href="#" class="title" data-abc="true">$5,500</a> <span class="rated">VISA Platinum</span> </div>
+            <div class="col-md-3 col-xs-3">
+              <div class="rating text-right"> #### 1333 </div>
+            </div>
+          </div>
+        </figcaption>
+      </div>
+      <div class="bottom-wrap"> <a href="#" class="btn btn-primary float-right" data-abc="true"> Buy now </a>
+        <div class="price-wrap"> <a href="#" class="btn btn-warning float-left" data-abc="true"> Cancel </a> </div>
+      </div>
+    </figure>
+
+
+
+
+    <figure class="card card-product-grid card-lg"> <a href="#" class="img-wrap" data-abc="true"> <img src="https://productimages.hepsiburada.net/s/34/1500/10443423055922.jpg"> </a>
+      <figcaption class="info-wrap">
+        <div class="row">
+          <div class="col-md-9 col-xs-9"> <a href="#" class="title" data-abc="true">Apple MacBook Pro</a> <span class="rated">Laptops</span> </div>
+          <div class="col-md-3 col-xs-3">
+            <div class="rating text-right"> <i class="fa fa-star"></i> <i class="fa fa-star"></i> <i class="fa fa-star"></i> <i class="fa fa-star"></i> <span class="rated">Rated 5.0/5</span> </div>
+          </div>
+        </div>
+      </figcaption>
+      <div class="bottom-wrap-payment">
+        <figcaption class="info-wrap">
+          <div class="row">
+            <div class="col-md-9 col-xs-9"> <a href="#" class="title" data-abc="true">$7,999</a> <span class="rated">VISA Platinum</span> </div>
+            <div class="col-md-3 col-xs-3">
+              <div class="rating text-right"> #### 9099 </div>
+            </div>
+          </div>
+        </figcaption>
+      </div>
+      <div class="bottom-wrap"> <a href="#" class="btn btn-primary float-right" data-abc="true"> Buy now </a>
+        <div class="price-wrap"> <a href="#" class="btn btn-warning float-left" data-abc="true"> Cancel </a> </div>
+      </div>
+    </figure>
+
+  </div>
+
+  <br> <br> <br>
+
+  <footer>
+    <!-- Copyrights -->
+    <div class="bg-light py-4">
+      <div class="container text-center">
+        <p class="text-muted mb-0 py-2"><b>Tk-Shopping</b></p>
+        <p class="text-muted mb-0 py-2">© 2022 All rights reserved.</p>
+      </div>
+    </div>
+  </footer>
+
+</template>
+
+<!--<script>
+import { computed, ref } from "vue";
+export default {
+  name: "ProductList",
+  setup() {
+    const cart = ref([]);
+    const products = ref([
+      {
+        id: 1,
+        name: "Product 1",
+        price: 1,
+        url:
+            "https://images.pexels.com/photos/59945/strawberry-fruit-delicious-red-59945.jpeg",
+        cart: false,
+        quantity: 1,
+      },
+      {
+        id: 2,
+        name: "Product 2",
+        price: 2,
+        url:
+            "https://images.pexels.com/photos/52533/orange-fruit-vitamins-healthy-eating-52533.jpeg",
+        cart: false,
+        quantity: 1,
+      },
+      {
+        id: 3,
+        name: "Product 3",
+        price: 3,
+        url:
+            "https://images.pexels.com/photos/51312/kiwi-fruit-vitamins-healthy-eating-51312.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+        cart: false,
+        quantity: 1,
+      },
+    ]);
+
+    function addProduct(product) {
+      cart.value.push(product);
+      products.value.map((p) => {
+        if (product.id == p.id) {
+          p.cart = !p.cart;
+        }
+      });
+    }
+    function removeProduct(product) {
+      cart.value = cart.value.filter((p) => p.id != product.id);
+      products.value.map((p) => {
+        if (product.id == p.id) {
+          p.cart = !p.cart;
+          p.quantity = 1;
+        }
+      });
+    }
+    function increaseQ(product) {
+      products.value.map((p) => {
+        if (product.id == p.id) {
+          p.quantity += 1;
+        }
+      });
+    }
+    function decreaseQ(product) {
+      products.value.map((p) => {
+        if (product.id == p.id && p.quantity > 1) {
+          p.quantity -= 1;
+        }
+      });
+    }
+    const totalCart = computed(() => {
+      let t = 0;
+      cart.value.forEach((e) => {
+        t += e.quantity * e.price;
+      });
+      return t;
+    });
+    return {
+      products,
+      addProduct,
+      increaseQ,
+      removeProduct,
+      decreaseQ,
+      cart,
+      totalCart,
+    };
+  },
+};
+</script>
+-->
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+body{background-color: #EEEEEE}a{text-decoration: none !important}.card-product-list, .card-product-grid{margin-bottom: 0}
+.card{width: 500px;position: relative;display: -webkit-box;display: -ms-flexbox;display: flex;-webkit-box-orient: vertical;-webkit-box-direction: normal;-ms-flex-direction: column;flex-direction: column;min-width: 0;word-wrap: break-word;background-color: #fff;background-clip: border-box;border: 1px solid rgba(0, 0, 0, 0.1);border-radius: 23px;margin-top: 50px}
+.card-product-grid:hover{-webkit-box-shadow: 0 4px 15px rgba(153, 153, 153, 0.3);box-shadow: 0 4px 15px rgba(153, 153, 153, 0.3);-webkit-transition: .3s;transition: .3s}
+.card-product-grid .img-wrap{border-radius: 0.2rem 0.2rem 0 0;height: 220px}
+.card .img-wrap{overflow: hidden}
+.card-lg .img-wrap{height: 280px}
+.card-product-grid .img-wrap{border-radius: 0.2rem 0.2rem 0 0;height: 275px;padding: 16px}[class*='card-product'] .img-wrap img{height: 100%;max-width: 100%;width: auto;display: inline-block;-o-object-fit: cover;object-fit: cover}
+.img-wrap{text-align: center;display: block}
+.card-product-grid .info-wrap{overflow: hidden;padding: 18px 20px}[class*='card-product'] a.title{color: #212529;display: block}
+.rating-stars{display: inline-block;vertical-align: middle;list-style: none;margin: 0;padding: 0;position: relative;white-space: nowrap;clear: both}
+.rating-stars li.stars-active{z-index: 2;position: absolute;top: 0;left: 0;overflow: hidden}
+.rating-stars li{display: block;text-overflow: clip;white-space: nowrap;z-index: 1}
+.card-product-grid .bottom-wrap{padding: 18px;border-top: 1px solid #e4e4e4}
+.bottom-wrap-payment{padding: 0px;border-top: 1px solid #e4e4e4}
+.rated{font-size: 10px;color: #b3b4b6}
+.btn{display: inline-block;font-weight: 600;color: #343a40;text-align: center;vertical-align: middle;-webkit-user-select: none;-moz-user-select: none;-ms-user-select: none;user-select: none;background-color: transparent;border: 1px solid transparent;padding: 0.45rem 0.85rem;font-size: 1rem;line-height: 1.5;border-radius: 0.2rem}
+.btn-primary{color: #fff;background-color: #3167eb;border-color: #3167eb}
+.fa{color: #FF5722}
+</style>
